f2347c9bbb2b67bca2b641f8a95679562e6a0f31
Franziska S <fra.se@posteo.net>
2022-07-01 21:07:54 +0200

    user gets notification when recommendation was added to the bubble

--- src/components/friends/Details.jsx
+++ src/components/friends/Details.jsx
@@ -13,10 +13,13 @@ export default function Details() {
   const { getBubbleById, updateBubble } = useBubbles();
 
   let currentNotifications = currentUser.notifications;
+  console.log(currentNotifications);
 
   const currentBubble = getBubbleById(currentNotifications[0].toBubble);
   const invitedByUser = findUserById(currentNotifications[0].invitedBy);
-
+  const newReco = currentUser.recos.find(
+    (reco) => reco.id === currentNotifications[0].recoId
+  );
   const resetInvitationsArr = () => {
     //delete latest notification
     currentUser.notifications.shift();
@@ -52,6 +55,36 @@ export default function Details() {
     <div className="flex items-center flex-col pt-28">
       <h1>WOW WOW WOW</h1>
       <p>here are your news</p>
+      {newReco && (
+        <div className="flex mt-8 justify-center">
+          <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-gradient-to-r from-cyan-500 to-blue-500 shadow-lg">
+            <img
+              className="h-28 md:h-auto opacity-60 object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+              src="https://images.unsplash.com/photo-1545315003-c5ad6226c272?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+              alt=""
+            />
+            <div className="p-6 flex flex-col bg- justify-start">
+              <h5 className="text-gray-900 uppercase text-xl font-medium mb-2">
+                You got a Reco: {newReco.title}
+              </h5>
+              <p className="text-gray-900 text-base mb-4">
+                Congratulations, you got a Reco by{" "}
+                {currentNotifications[0].invitedByUser}!
+              </p>
+              <div className="text-black flex justify-end gap-3">
+                <VscChromeClose
+                  onClick={() => refuseBubbleInvitation()}
+                  className="cursor-pointer"
+                ></VscChromeClose>
+                <VscCheck
+                  onClick={() => {}}
+                  className="cursor-pointer"
+                ></VscCheck>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
       {currentBubble && (
         <div className="flex mt-8 justify-center">
           <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-gradient-to-r from-cyan-500 to-blue-500 shadow-lg">
