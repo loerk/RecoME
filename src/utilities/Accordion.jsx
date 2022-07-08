1f3394a4a88e4a1f4f3a7410e34647a779e79e9f
Franziska S <fra.se@posteo.net>
2022-07-08 20:26:13 +0200

    renamed variables

--- src/utilities/Accordion.jsx
+++ src/utilities/Accordion.jsx
@@ -4,24 +4,26 @@ import { BsChevronUp, BsChevronDown } from "react-icons/bs";
 import { LinkPreview } from "./LinkPreview";
 
 export default function Accordion({ title, date, comment, content }) {
-  const [isOpened, setOpened] = useState(false);
-  const [height, setHeight] = useState("0px");
+  const [isOpen, setIsOpen] = useState(false);
   const contentElement = useRef(null);
 
-  const handleOpening = () => {
-    setOpened(!isOpened);
-    setHeight(!isOpened ? `${contentElement.current.scrollHeight}px` : "0px");
+  const handleClick = () => {
+    setIsOpen(!isOpen);
   };
   return (
-    <div onClick={handleOpening} className="w-full bg-yellow-100">
+    <div onClick={handleClick} className="w-full bg-yellow-100">
       <div className="p-4 flex bg-yellow-200  justify-between">
         <h4 className="text-sm mt-auto">{date}</h4>
         <h4>{title}</h4>
-        {isOpened ? <BsChevronUp /> : <BsChevronDown />}
+        {isOpen ? <BsChevronUp /> : <BsChevronDown />}
       </div>
       <div
         ref={contentElement}
-        style={{ height: height }}
+        style={
+          isOpen
+            ? { height: contentElement.current.scrollHeight }
+            : { height: "0px" }
+        }
         className="overflow-hidden transition-all duration-200"
       >
         <p className="p-4">{comment}</p>
