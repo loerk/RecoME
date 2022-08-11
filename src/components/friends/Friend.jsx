4d2b07723430b5aab6147f2492645d6c97263506
Franziska S <fra.se@posteo.net>
2022-08-11 20:20:55 +0200

    remove friend

--- src/components/friends/Friend.jsx
+++ src/components/friends/Friend.jsx
@@ -1,23 +1,36 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useUsers } from "../../contexts/UsersContext";
 
 export default function Friend() {
+  const navigate = useNavigate();
   const { friendId } = useParams();
+  const { friends, unfriendUser, setShouldUpdateFriends } = useUsers();
+  const friend = friends.find((friend) => friend._id === friendId);
 
-  const { currentUser } = useUsers();
-  const currentFriend = currentUser.friends.find(
-    (friend) => friend._id === friendId
-  );
+  const removeFriend = async () => {
+    await unfriendUser(friendId);
+    setShouldUpdateFriends(true);
+    navigate("/friends");
+  };
 
   return (
     <div className="flex m-10">
-      <img src={currentFriend.avatarUrl} alt="friendAvatar" />
+      <img src={friend.avatarUrl} alt="friendAvatar" />
       <div>
+        <h2 className="tracking-widest pb-4">
+          {friend.username.toUpperCase()}
+        </h2>
         <div>
           <h2>shared Bubbles</h2>
           <h2>shared Recommendations </h2>
         </div>
+        <button
+          className="inline-block leading-tight uppercase  shadow-md hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:shadow-lg focus:bg-black focus:shadow-lg focus:outline-none focus:ring-0 active:bg-pink-800 active:shadow-lg transition duration-150 ease-in-out bg-black rounded-md  text-white px-2 py-1 my-4"
+          onClick={() => removeFriend()}
+        >
+          remove friend
+        </button>
       </div>
     </div>
   );
