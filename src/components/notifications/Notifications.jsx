5d08018a13c35411cface4fb24ee962cc197b60d
Franziska S <fra.se@posteo.net>
2022-08-23 08:42:20 +0200

    added option if no notifications left

--- src/components/notifications/Notifications.jsx
+++ src/components/notifications/Notifications.jsx
@@ -14,37 +14,41 @@ export default function Notifications() {
     <div className="flex items-center flex-col pt-32 pb-12">
       <h1>WOW WOW WOW</h1>
       <p>here are your news</p>
-      {notifications.map((notification) => {
-        if (notification.type === NotificationType.INVITATION_TO_RECO) {
-          return (
-            <RecommendationNotification
-              key={notification._id}
-              notification={notification}
-            />
-          );
-        }
-        if (notification.type === NotificationType.INVITATION_TO_BUBBLE) {
-          return (
-            <BubbleNotification
-              key={notification._id}
-              notification={notification}
-            />
-          );
-        }
-        if (
-          notification.type ===
-          NotificationType.NOTIFICATION_ABOUT_RECO_IN_BUBBLE
-        ) {
-          return (
-            <RecommendationToBubbleNotification
-              key={notification._id}
-              notification={notification}
-            />
-          );
-        }
+      {notifications.length ? (
+        notifications.map((notification) => {
+          if (notification.type === NotificationType.INVITATION_TO_RECO) {
+            return (
+              <RecommendationNotification
+                key={notification._id}
+                notification={notification}
+              />
+            );
+          }
+          if (notification.type === NotificationType.INVITATION_TO_BUBBLE) {
+            return (
+              <BubbleNotification
+                key={notification._id}
+                notification={notification}
+              />
+            );
+          }
+          if (
+            notification.type ===
+            NotificationType.NOTIFICATION_ABOUT_RECO_IN_BUBBLE
+          ) {
+            return (
+              <RecommendationToBubbleNotification
+                key={notification._id}
+                notification={notification}
+              />
+            );
+          }
 
-        return null;
-      })}
+          return null;
+        })
+      ) : (
+        <h1 className="mt-10"> : - / </h1>
+      )}
     </div>
   );
 }
