a0e6cf1e575db074521508f08764eae5712e9204
Franziska S <fra.se@posteo.net>
2022-08-23 13:04:34 +0200

    fallback for Linkpreview

--- src/utilities/LinkPreview.jsx
+++ src/utilities/LinkPreview.jsx
@@ -32,9 +32,17 @@ export function LinkPreview({ url }) {
           href={url}
           target="_blank"
           rel="noopener noreferrer"
-          className="cursor:pointer flex p-2 my-2 gap-4 md:w-56 w-52"
+          className="cursor:pointer  bg-purple-100 flex p-2 my-2 gap-4 md:w-56 w-52"
         >
           <img className="object-cover w-16 h-16" src={abstract} alt="" />
+          <div>
+            <h3 className="text-xs truncate md:text-sm text-ellipsis">
+              {"no further information available"}
+            </h3>
+            <p className="truncate text-xs p-2 text-ellipsis">
+              {"the link should work :)"}
+            </p>
+          </div>
         </a>
       )}
     </div>
