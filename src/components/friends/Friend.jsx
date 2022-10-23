da5e88470ae0fe04852e7568b3f80b1b4f715764
Franziska S <fra.se@posteo.net>
2022-10-23 18:03:01 +0200

    rename button

--- src/components/friends/Friend.jsx
+++ src/components/friends/Friend.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { useUsers } from "../../contexts/UsersContext";
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useUsers } from '../../contexts/UsersContext';
 
 export default function Friend() {
   const navigate = useNavigate();
@@ -11,14 +11,14 @@ export default function Friend() {
   const removeFriend = async () => {
     await unfriendUser(friendId);
     setShouldUpdateFriends(true);
-    navigate("/friends");
+    navigate('/friends');
   };
 
   return (
-    <div className="md:flex m-10">
-      <img src={friend.avatarUrl} alt="friendAvatar" />
+    <div className='md:flex m-10'>
+      <img src={friend.avatarUrl} alt='friendAvatar' />
       <div>
-        <h2 className="tracking-widest pb-4">
+        <h2 className='tracking-widest pb-4'>
           {friend.username.toUpperCase()}
         </h2>
         <div>
@@ -26,10 +26,10 @@ export default function Friend() {
           <h2>shared Recommendations </h2>
         </div>
         <button
-          className="inline-block leading-tight uppercase  shadow-md hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:shadow-lg focus:bg-black focus:shadow-lg focus:outline-none focus:ring-0 active:bg-pink-800 active:shadow-lg transition duration-150 ease-in-out bg-black rounded-md  text-white px-2 py-1 my-4"
+          className='inline-block leading-tight uppercase  shadow-md hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:shadow-lg focus:bg-black focus:shadow-lg focus:outline-none focus:ring-0 active:bg-pink-800 active:shadow-lg transition duration-150 ease-in-out bg-black rounded-md  text-white px-2 py-1 my-4'
           onClick={() => removeFriend()}
         >
-          remove friend
+          unfriend
         </button>
       </div>
     </div>
