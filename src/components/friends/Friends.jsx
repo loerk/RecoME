fb706bc73f21bcb7604966dbc03697858b36b2fa
Franziska S <fra.se@posteo.net>
2022-10-23 18:02:29 +0200

    added marginX

--- src/components/friends/Friends.jsx
+++ src/components/friends/Friends.jsx
@@ -1,14 +1,14 @@
-import { useNavigate, Outlet, useLocation } from "react-router-dom";
-import { AddButton } from "../../utilities/Buttons";
-import { useUsers } from "../../contexts/UsersContext";
-import React, { useEffect, useState } from "react";
+import { useNavigate, Outlet, useLocation } from 'react-router-dom';
+import { AddButton } from '../../utilities/Buttons';
+import { useUsers } from '../../contexts/UsersContext';
+import React, { useEffect, useState } from 'react';
 
 export default function Friends() {
   const navigate = useNavigate();
   const location = useLocation();
   window.scrollTo(0, 0);
 
-  const [searchFor, setSearchFor] = useState("");
+  const [searchFor, setSearchFor] = useState('');
   const { friends, setShouldUpdateFriends } = useUsers();
 
   useEffect(() => {
@@ -17,19 +17,19 @@ export default function Friends() {
   }, [friends]);
 
   const addFriend = () => {
-    navigate("/friends/addFriend");
+    navigate('/friends/addFriend');
   };
 
   return (
-    <div className="pt-32 pb-12">
-      {location.pathname === "/friends" ? (
+    <div className='pt-32 pb-12 mx-24'>
+      {location.pathname === '/friends' ? (
         <div>
-          <div className="flex justify-center">
-            <div className="mb-3 xl:w-96">
+          <div className='flex justify-center'>
+            <div className='mb-3 xl:w-96'>
               <input
                 onChange={(e) => setSearchFor(e.target.value)}
-                type="search"
-                className="
+                type='search'
+                className='
                     text-center
                     form-control
                     block
@@ -46,9 +46,9 @@ export default function Friends() {
                     ease-in-out
                     m-0
                     focus:text-gray-700 focus:bg-white focus:border-slate-600 focus:outline-none
-                  "
-                id="exampleSearch"
-                placeholder="Search Friend"
+                  '
+                id='exampleSearch'
+                placeholder='Search Friend'
               />
             </div>
           </div>
@@ -57,7 +57,7 @@ export default function Friends() {
 
           <div>
             {friends.length !== 0 ? (
-              <ul className="pt-6 flex flex-wrap gap-4 justify-around">
+              <ul className='pt-6 flex flex-wrap gap-4 justify-around'>
                 {friends
                   .filter((friend) => {
                     return friend.username
@@ -67,21 +67,21 @@ export default function Friends() {
                   .map((friend) => {
                     return (
                       <li key={friend._id}>
-                        <div className="text-center">
+                        <div className='text-center'>
                           <img
                             onClick={() => navigate(`/friends/${friend._id}`)}
-                            className="w-28 h-28 object-cover object-center opacity-50  hover:opacity-100 rounded-full cursor-pointer"
+                            className='w-28 h-28 object-cover object-center opacity-50  hover:opacity-100 rounded-full cursor-pointer'
                             src={friend.avatarUrl}
-                            alt=""
+                            alt=''
                           />
-                          <p className="relative">{friend.username}</p>
+                          <p className='relative'>{friend.username}</p>
                         </div>
                       </li>
                     );
                   })}
               </ul>
             ) : (
-              <p className="text-center pt-5">
+              <p className='text-center pt-5'>
                 :/ you dont have any friends...yet
               </p>
             )}
