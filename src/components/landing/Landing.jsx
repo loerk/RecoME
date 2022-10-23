5ae14005d62a16bcfde13dd3188167a302d76a2e
Franziska S <fra.se@posteo.net>
2022-10-23 15:49:10 +0200

    truncate titles and added horizontal scroll

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -27,11 +27,11 @@ export default function Landing() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
-    <div className="pb-12 pt-32">
-      <div className="flex items-center m-auto gap-7 mt-16 py-5 flex-col h-76 rounded shadow-2xl ">
-        <div className="flex justify-around items-center w-full content-center">
-          <h1 className="px-10">Your bubbles</h1>
-          <ul className="flex overflow-x-hidden w-2/3">
+    <div className='pb-12 pt-32'>
+      <div className='flex items-center m-auto gap-7 mt-16 py-5 flex-col h-76 rounded shadow-2xl '>
+        <div className='flex justify-around items-center w-full content-center'>
+          <h1 className='px-10'>Your bubbles</h1>
+          <ul className='flex overflow-x-scroll w-2/3'>
             {bubbles?.map((bubble) => (
               <li key={bubble._id}>
                 <div className='text-center m-2'>
@@ -44,56 +44,58 @@ export default function Landing() {
                       alt='costum background'
                     />
                   </div>
-                  <button>{bubble.name}</button>
+                  <button className=' w-28 truncate px-3'>{bubble.name}</button>
                 </div>
               </li>
             ))}
           </ul>
-          <Link to={"/bubbles"}>
-            <MdArrowForwardIos className="mx-6 text-2xl md:text-3xl"></MdArrowForwardIos>
+          <Link to={'/bubbles'}>
+            <MdArrowForwardIos className='mx-6 text-2xl md:text-3xl'></MdArrowForwardIos>
           </Link>
         </div>
       </div>
-      <div className="flex items-center m-auto gap-7 my-7 py-5 flex-col h-76  rounded shadow-2xl ">
-        <div className="flex justify-around items-center w-full content-center">
-          <h1 className="px-10">Your friends</h1>
-          <ul className="flex overflow-x-hidden w-2/3">
+      <div className='flex items-center m-auto gap-7 my-7 py-5 flex-col h-76  rounded shadow-2xl '>
+        <div className='flex justify-around items-center w-full content-center'>
+          <h1 className='px-10'>Your friends</h1>
+          <ul className='flex overflow-x-scroll w-2/3'>
             {friends?.map((friend) => {
               return (
                 <li key={friend._id}>
-                  <div className="text-center m-2">
-                    <div className="w-28 h-28 object-cover object-center opacity-50  hover:opacity-100 rounded-full cursor-pointer">
+                  <div className='text-center m-2'>
+                    <div className='w-28 h-28 object-cover object-center opacity-50  hover:opacity-100 rounded-full cursor-pointer'>
                       <img
                         onClick={() => navigate(`/friends/${friend._id}`)}
                         src={friend.avatarUrl}
-                        alt=""
+                        alt=''
                       />
                     </div>
-                    <button className="relative">{friend.username}</button>
+                    <button className='relative truncate px-3'>
+                      {friend.username}
+                    </button>
                   </div>
                 </li>
               );
             })}
           </ul>
-          <Link to={"/friends"}>
-            <MdArrowForwardIos className="mx-6 text-2xl md:text-3xl"></MdArrowForwardIos>
+          <Link to={'/friends'}>
+            <MdArrowForwardIos className='mx-6 text-2xl md:text-3xl'></MdArrowForwardIos>
           </Link>
         </div>
-      </div>{" "}
-      <div className="flex items-center m-auto gap-7 my-7 py-5 flex-col h-76rounded shadow-2xl ">
-        <div className="flex justify-around items-center w-full content-center">
-          <h1 className="px-10">Latest Recos</h1>
-          <ul className="flex overflow-x-hidden w-2/3">
+      </div>{' '}
+      <div className='flex items-center m-auto gap-7 my-7 py-5 flex-col h-76 rounded shadow-2xl '>
+        <div className='flex justify-around items-center w-full content-center'>
+          <h1 className='px-10'>Latest Recos</h1>
+          <ul className='flex overflow-x-scroll w-2/3'>
             {recos?.map((reco) => (
-              <button key={reco._id} onClick={() => navigate("/recos")}>
-                <div className="flex m-2 items-center w-28 h-28 opacity-60 bg-yellow-200  hover:opacity-100 rounded-full cursor-pointer">
-                  <div className="truncate px-3 ">{reco.title}</div>
+              <button key={reco._id} onClick={() => navigate('/recos')}>
+                <div className='flex m-2 items-center w-28 h-28 opacity-60 bg-yellow-200  hover:opacity-100 rounded-full cursor-pointer'>
+                  <div className='truncate px-3 '>{reco.title}</div>
                 </div>
               </button>
             ))}
           </ul>
-          <Link to={"/recos"}>
-            <MdArrowForwardIos className="mx-6 text-2xl md:text-3xl"></MdArrowForwardIos>
+          <Link to={'/recos'}>
+            <MdArrowForwardIos className='mx-6 text-2xl md:text-3xl'></MdArrowForwardIos>
           </Link>
         </div>
       </div>
