e07865f8c5b0fc249f970636f6994824478d83db
Franziska S <fra.se@posteo.net>
2022-10-24 18:36:51 +0200

    conditional rendering recos

--- src/components/recommendations/Recos.jsx
+++ src/components/recommendations/Recos.jsx
@@ -1,11 +1,5 @@
 import React, { useState } from 'react';
-import {
-  Outlet,
-  useLocation,
-  Link,
-  Navigate,
-  useNavigate,
-} from 'react-router-dom';
+import { Outlet, useLocation, Link, useNavigate } from 'react-router-dom';
 import { RiDeleteBinLine } from 'react-icons/ri';
 import { v1 as uuidv1 } from 'uuid';
 import { AiOutlineHeart } from 'react-icons/ai';
@@ -32,17 +26,19 @@ export default function Recos() {
     setShowModal(true);
     setRecoIdToDelete(id);
   };
+  if (isLoadingRecos) return <div>Loading</div>;
   if (!recos) return;
-  console.log(recos);
+
   return (
     <div className='pt-32 pb-12'>
-      <div>
-        <div className='flex justify-center'>
-          <div className='mb-3 xl:w-96'>
-            <input
-              onChange={(e) => setSearchFor(e.target.value)}
-              type='search'
-              className='
+      {location.pathname === '/recos' ? (
+        <div>
+          <div className='flex justify-center'>
+            <div className='mb-3 xl:w-96'>
+              <input
+                onChange={(e) => setSearchFor(e.target.value)}
+                type='search'
+                className='
                   text-center
                   form-control
                   block
@@ -60,112 +56,115 @@ export default function Recos() {
                   m-0
                   focus:text-gray-700 focus:bg-white focus:outline-none
                 '
-              id='exampleSearch'
-              placeholder='Search Recs'
-            />
+                id='exampleSearch'
+                placeholder='Search Recs'
+              />
+            </div>
           </div>
-        </div>
 
-        <Link to='/recos/addReco'>
-          <AddButton />
-        </Link>
+          <Link to='/recos/addReco'>
+            <AddButton />
+          </Link>
 
-        {recos.length ? (
-          <ul className='pt-6 p-10 flex flex-wrap gap-4 justify-around'>
-            {recos
-              ?.filter((reco) => {
-                return reco.title
-                  .toLowerCase()
-                  .includes(searchFor.toLowerCase());
-              })
-              .map((reco) => {
-                const date = moment(reco.createdAt).fromNow();
+          {recos.length ? (
+            <ul className='pt-6 p-10 flex flex-wrap gap-4 justify-around'>
+              {recos
+                ?.filter((reco) => {
+                  return reco.title
+                    .toLowerCase()
+                    .includes(searchFor.toLowerCase());
+                })
+                .map((reco) => {
+                  const date = moment(reco.createdAt).fromNow();
 
-                return (
-                  <div key={uuidv1()}>
-                    <div className='flex w-64 flex-col hover:shadow-inner md:flex-row  rounded-lg shadow-lg  '>
-                      <div className='relative w-full rounded flex flex-col justify-start'>
-                        <div className='z-2 flex justify-between'>
-                          <div className='p-4'>
-                            <p className='tracking-widest text-xs'>{date}</p>
-                            <h5 className='text-xl w-24 break-words font-medium mb-2'>
-                              {reco.title}
-                            </h5>
-                          </div>
-                          <div className='relative flex p-4'>
-                            <img
-                              onClick={() =>
-                                navigate(`/friends/${reco?.createdBy?._id}`)
-                              }
-                              src={reco?.createdBy?.avatarUrl || bubbleImg}
-                              alt=''
-                              className='w-9 h-9 z-3 relative hover:scale-110 duration-300 left-3 shadow-lg cursor-pointer rounded-full'
-                            />
-                            {reco.bubbleId ? (
+                  return (
+                    <div key={uuidv1()}>
+                      <div className='flex w-64 flex-col hover:shadow-inner md:flex-row  rounded-lg shadow-lg  '>
+                        <div className='relative w-full rounded flex flex-col justify-start'>
+                          <div className='z-2 flex justify-between'>
+                            <div className='p-4'>
+                              <p className='tracking-widest text-xs'>{date}</p>
+                              <h5 className='text-xl w-24 break-words font-medium mb-2'>
+                                {reco.title}
+                              </h5>
+                            </div>
+                            <div className='relative flex p-4'>
                               <img
                                 onClick={() =>
-                                  navigate(`/bubbles/${reco.bubbleId}`)
+                                  navigate(`/friends/${reco?.createdBy?._id}`)
                                 }
-                                src={reco.bubbleId.imageUrl || bubbleImg}
+                                src={reco?.createdBy?.avatarUrl || bubbleImg}
+                                alt='user avatar'
                                 onError={(e) => onImageError(e)}
-                                alt=''
-                                className='w-9 h-9 object-cover cursor-pointer shadow-lg rounded-full hover:scale-110 duration-300'
+                                className='w-9 h-9 z-3 relative hover:scale-110 duration-300 left-3 shadow-lg cursor-pointer rounded-full'
                               />
-                            ) : (
-                              <img
-                                onClick={() =>
-                                  navigate(`/friends/${reco.userIds[0]}`)
-                                }
-                                src={reco?.userIds[0]?.avatarUrl || bubbleImg}
-                                alt=''
-                                className='w-9 h-9 shadow-lg cursor-pointer rounded-full hover:scale-110 duration-300'
-                              />
-                            )}
+                              {reco.bubbleId ? (
+                                <img
+                                  onClick={() =>
+                                    navigate(`/bubbles/${reco.bubbleId}`)
+                                  }
+                                  src={reco.bubbleId.imageUrl || bubbleImg}
+                                  onError={(e) => onImageError(e)}
+                                  alt='bubble background'
+                                  className='w-9 h-9 object-cover cursor-pointer shadow-lg rounded-full hover:scale-110 duration-300'
+                                />
+                              ) : (
+                                <img
+                                  onClick={() =>
+                                    navigate(`/friends/${reco.userIds[0]}`)
+                                  }
+                                  src={reco?.userIds[0]?.avatarUrl || bubbleImg}
+                                  alt='user avatar'
+                                  onError={(e) => onImageError(e)}
+                                  className='w-9 h-9 shadow-lg cursor-pointer rounded-full hover:scale-110 duration-300'
+                                />
+                              )}
+                            </div>
+                          </div>
+                          <p className='text-base pl-4 pb-4'>
+                            {reco.description}
+                          </p>
+                          <div className='h-24 w-52 flex'>
+                            <div className='h-24 mr-5 bg-black'>l</div>
+                            <LinkPreview url={reco.recoUrl} />
+                          </div>
+                          <div className='flex flex-wrap  gap-2 justify-center mt-auto'>
+                            {reco.categories.map((category) => {
+                              return (
+                                <span
+                                  key={uuidv1()}
+                                  className='text-xs tracking-widest font-face-tl inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-lime-400 text-black rounded-full'
+                                >
+                                  {category}
+                                </span>
+                              );
+                            })}
+                          </div>
+                          <div className='flex m-4 mt-8 justify-center gap-4'>
+                            <RiDeleteBinLine
+                              className='cursor-pointer'
+                              onClick={() => handleModal(reco._id)}
+                            ></RiDeleteBinLine>
+                            <AiOutlineHeart className='cursor-pointer'></AiOutlineHeart>
                           </div>
                         </div>
-                        <p className='text-base pl-4 pb-4'>
-                          {reco.description}
-                        </p>
-                        <div className='h-24 w-52 flex'>
-                          <div className='h-24 mr-5 bg-black'>l</div>
-                          <LinkPreview url={reco.recoUrl} />
-                        </div>
-                        <div className='flex flex-wrap  gap-2 justify-center mt-auto'>
-                          {reco.categories.map((category) => {
-                            return (
-                              <span
-                                key={uuidv1()}
-                                className='text-xs tracking-widest font-face-tl inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-lime-400 text-black rounded-full'
-                              >
-                                {category}
-                              </span>
-                            );
-                          })}
-                        </div>
-                        <div className='flex m-4 mt-8 justify-center gap-4'>
-                          <RiDeleteBinLine
-                            className='cursor-pointer'
-                            onClick={() => handleModal(reco._id)}
-                          ></RiDeleteBinLine>
-                          <AiOutlineHeart className='cursor-pointer'></AiOutlineHeart>
-                        </div>
+                        {showModal && (
+                          <DeleteRecoModal
+                            showModal={showModal}
+                            setShowModal={setShowModal}
+                            recoId={recoIdToDelete}
+                          />
+                        )}
                       </div>
-                      {showModal && (
-                        <DeleteRecoModal
-                          showModal={showModal}
-                          setShowModal={setShowModal}
-                          recoId={recoIdToDelete}
-                        />
-                      )}
                     </div>
-                  </div>
-                );
-              })}
-          </ul>
-        ) : (
-          <p className='text-center'>:/ you didnt write any recos...yet</p>
-        )}
-      </div>
+                  );
+                })}
+            </ul>
+          ) : (
+            <p className='text-center'>:/ you didnt write any recos...yet</p>
+          )}
+        </div>
+      ) : null}
 
       <Outlet />
     </div>
